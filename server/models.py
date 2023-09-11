8e64264f5d9e9358ee599a1b6ae6b40e2e39dc2d
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-11 13:58:46 -0400

    Complete

--- server/models.py
+++ server/models.py
@@ -11,6 +11,19 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+   
+    @validates('name')
+    def validates_name(self,key,value):
+        if value =="":
+            raise ValueError('Name cannot be blank')
+        return value
+    
+    @validates('phone_number')
+    def validate_phone_number(self,key,phone_number):
+        if len(phone_number) != 10:
+            raise ValueError('Phone number must have 10 digits.')
+        else:
+            return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,6 +40,34 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(self,key,content):
+        if len(content) < 250:
+            raise ValueError('Content must be longer than 250 characters')
+        else:
+            return content
+    
+    @validates('summary')
+    def validate_summary(self,key,summary):
+        if len(summary) >= 250:
+            raise ValueError('Summary must be longer than 250 characters')
+        else:
+            return summary
+        
+    @validates('category')
+    def validates_category(self, key, category):
+        categories = ['Fiction', 'Non-Fiction']
+        if category not in categories:
+            raise ValueError('Category must either be Fiction or Non-Fiction.')
+        else:
+            return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
+    @validates('title')
+    def validates_click_bait(self, key, value):
+        clickbait_keywords = ["Yes", "No"]
+        if value not in clickbait_keywords:
+            raise ValueError('Clickbait value must be one of the keywords')
+        return value
